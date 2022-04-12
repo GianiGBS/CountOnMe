9cb7e9515e6bc73266601ebea643676d6ac1006f
GiovanniGBS <giovannigbs@icloud.com>
2022-04-12 11:27:04 -0400

    Design + Functionality : Operator

---
+++ CountOnMe/Calculator.swift
@@ -0,0 +1,12 @@
+//
+//  Calculator.swift
+//  CountOnMe
+//
+//  Created by Giovanni Gabriel on 10/04/2022.
+//  Copyright © 2022 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+
+class Calculator {
+}
